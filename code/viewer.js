4824af0290b045106c56bccd217ce3a75b899182
xizhonghua <xizhonghua@gmail.com>
2014-02-04 01:03:37 -0500

    update viewer

--- code/viewer.js
+++ code/viewer.js
@@ -1,6 +1,113 @@
 chrome.runtime.sendMessage({action: "getOptions"}, function(options) {
 	if(!/https:\/\/bbs\.sjtu\.edu\.cn\/bbsfdoc2\?/.test(window.location.href)
 	&& !/https:\/\/bbs\.sjtu\.cn\/bbsfdoc2\?/.test(window.location.href)) return;
+
+	var state  = {
+		timeout : -1,
+		currentImgIndex : -1,
+		lastPrev : -1,
+		lastNext : -1
+	};
+
+	function clickElement(ele) {
+		var e = document.createEvent('MouseEvents');
+		e.initEvent('click', true, true );
+		ele.dispatchEvent(e);
+	}
+	
+	function scrollToImg() {
+		if(imgs.length == 0) return;
+		$('html, body').animate({
+    		scrollTop: (imgs[state.currentImgIndex].first().offset().top)
+		}, 150);
+	}
+	
+	function nextPage() {
+		var ele = $("a:contains('下一页')")[0];
+		if(ele) {
+			clickElement(ele);
+			showHint('Loading next page...');
+		}
+		else {
+			showHint('No more pages...');
+		}
+	}
+	
+	function prevPage() {
+		var ele = $("a:contains('上一页')")[0];
+		if(ele) {
+			clickElement(ele);
+			showHint('Loading previous page...');
+		}
+		else {
+			showHint('No more pages...');
+		}
+	}
+	
+	function prevImg() {
+		if(state.currentImgIndex > 0) {
+			state.currentImgIndex --;
+			scrollToImg();
+		} else {
+			var n = new Date().getTime();
+			if(n - state.lastPrev < 2000) {
+				nextPage();
+			} else {
+				showHint("Press K to load more...");
+			}
+			state.lastPrev = n;
+		}
+	}
+	
+	function nextImg() {
+		if(state.currentImgIndex < imgs.length - 1) {
+			state.currentImgIndex++;
+			scrollToImg();
+		} else {
+			var n = new Date().getTime();
+			if(n - state.lastNext < 2000) {
+				prevPage();
+			} else {
+				showHint("Press J to load more...");
+			}
+			state.lastNext = n;
+		}
+	}
+	
+	$(window).keydown(function(event){
+		var ele, img;
+		switch(event.keyCode)
+		{	 
+			case 37:	// <- : previous page
+			case 72:	// h  : previous page
+				prevPage();
+				break;
+			case 39:	// -> : next page
+			case 76:	// l  : next page
+				nextPage();
+				break;
+			case 75:	// k : previous image
+				prevImg();
+				break;
+			case 74:	// j : next image
+				nextImg();
+				break;
+			case 84:	// t : test
+				showHint('testing...');
+				break;
+		}
+	});
+	
+	function showHint(msg) {
+		$hint.html(msg).css({'margin-right' : -$hint.width()/2 + 'px'}).show();
+		clearTimeout(state.timeout);
+		state.timeout = setTimeout(function() {
+			$hint.slideUp();
+		}, 2000);
+	}
+	
+	
+	var $hint = $('<div></div>').addClass('hint').text('hint').appendTo($("body")).hide();
 	
 	if(options.autoSize)
 		$('table').width($(window).width() - 60);
@@ -16,7 +123,7 @@ chrome.runtime.sendMessage({action: "getOptions"}, function(options) {
 	var imgMargin = options.hideColumn ? 240 : 360;
 	
 	var imgs = [];
-	var currentImgIndex = -1;
+	
 	
 	$('td > a').each(function(){
 			var href = $(this).attr("href");
@@ -34,41 +141,5 @@ chrome.runtime.sendMessage({action: "getOptions"}, function(options) {
 	$("a:contains('上一页')").text("上一页 ← ");
 	$("a:contains('下一页')").text("下一页 → ");
 	
-	function clickElement(ele) {
-		var e = document.createEvent('MouseEvents');
-		e.initEvent('click', true, true );
-		ele.dispatchEvent(e);
-	}
 	
-	function scrollToImg() {
-		if(imgs.length == 0) return;
-		$('html, body').animate({
-    		scrollTop: (imgs[currentImgIndex].first().offset().top)
-		},150);
-	}
-	
-	$(window).keydown(function(event){
-		var ele, img;
-		switch(event.keyCode)
-		{	 
-			case 37:	// <- : previous page
-			case 72:	// h  : previous page
-				ele = $("a:contains('上一页')")[0];
-				break;
-			case 39:	// -> : next page
-			case 76:	// l  : next page
-				ele = $("a:contains('下一页')")[0];				
-				break;
-			case 75:	// k : previous image
-				if(currentImgIndex > 0) currentImgIndex --;
-				scrollToImg();
-				break;
-			case 74:	// j : next image
-				if(currentImgIndex < imgs.length - 1) currentImgIndex++;
-				scrollToImg();
-				break;
-		}			
-		
-		if(ele) clickElement(ele); 
-   });
 });
